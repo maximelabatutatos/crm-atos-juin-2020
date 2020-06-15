50cfb6fa427f47dee355135efffe54b54d97f3af
maxime.labatut <maxime.labatut@atos.net>
2020-06-15 17:16:13 +0200

    IconsModule

---
+++ src/app/icons/components/icons/icon-close.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-icon-close',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss']
+})
+export class IconCloseComponent implements OnInit {
+
+  public myIcon = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
